669234674f4c5ef77d2c739822ab9974a9763c47
jeheecheon <jeheecheon@gmail.com>
2026-02-17 18:20:31 +0900

    chore: enforce absolute imports via eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-plugin-prettier/recommended'
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -17,12 +18,19 @@ export default defineConfig([
       reactRefresh.configs.vite,
       prettier,
     ],
+    plugins: {
+      'no-relative-import-paths': noRelativeImportPaths,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
       "react-refresh/only-export-components": "off",
+      "no-relative-import-paths/no-relative-import-paths": [
+        "error",
+        { "allowSameFolder": false, "prefix": "@" },
+      ],
     },
   },
 ])
--- src/root.tsx
+++ src/root.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
-import { Layout } from "./layouts/RootLayout";
-import { HydrateFallback } from "./components/HydrateFallback";
-import { config } from "./wagmi";
-import "./index.css";
+import { Layout } from "@/layouts/RootLayout";
+import { HydrateFallback } from "@/components/HydrateFallback";
+import { config } from "@/wagmi";
+import "@/index.css";
 
 const queryClient = new QueryClient();
 
