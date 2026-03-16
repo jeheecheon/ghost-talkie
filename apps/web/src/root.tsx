8c2876183012e964d840992f95840e6ada5d0665
jeheecheon <jeheecheon@gmail.com>
2026-03-17 07:06:25 +0900

    chore: remove debug profile links from root layout

--- apps/web/src/root.tsx
+++ apps/web/src/root.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router";
+import { Outlet } from "react-router";
 import { Layout } from "@/layout";
 import ErrorBoundary from "@/error-boundary";
 import RootProviders from "@/providers/root-providers";
@@ -10,10 +10,6 @@ export { Layout, ErrorBoundary };
 export default function Root() {
   return (
     <RootProviders>
-      <section>
-        <Link to="/0xE7271a41640e91616674fcCB7F46Bd61d7815E30">Profile 1</Link>
-        <Link to="/0xd76bc6C88DBf1923343BCCb2E8ed730d27597EfF">Profile 2</Link>
-      </section>
       <Outlet />
       <ChatWidget />
     </RootProviders>
