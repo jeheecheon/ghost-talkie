003fa0e07b07314b0ff68ca67b777ffb852abe71
jeheecheon <jeheecheon@gmail.com>
2026-03-16 21:55:07 +0900

    feat: add testnet chain icons and fallback to ChainIcon

--- packages/ui/src/icons/chain-icon.tsx
+++ packages/ui/src/icons/chain-icon.tsx
@@ -1,18 +1,22 @@
-import type { ComponentType } from "react";
+import {
+  mainnet,
+  arbitrum,
+  base,
+  optimism,
+  polygon,
+  sepolia,
+  arbitrumSepolia,
+  baseSepolia,
+  optimismSepolia,
+  polygonAmoy,
+} from "viem/chains";
 import { cn } from "@workspace/lib/cn";
-import EthereumIcon from "./ethereum-icon";
-import ArbitrumIcon from "./arbitrum-icon";
-import BaseIcon from "./base-icon";
-import OptimismIcon from "./optimism-icon";
-import PolygonIcon from "./polygon-icon";
-
-const CHAIN_ICONS: Record<number, ComponentType<{ className?: string }>> = {
-  1: EthereumIcon,
-  42161: ArbitrumIcon,
-  8453: BaseIcon,
-  10: OptimismIcon,
-  137: PolygonIcon,
-};
+import { Circle } from "lucide-react";
+import EthereumIcon from "@workspace/ui/icons/ethereum-icon";
+import ArbitrumIcon from "@workspace/ui/icons/arbitrum-icon";
+import BaseIcon from "@workspace/ui/icons/base-icon";
+import OptimismIcon from "@workspace/ui/icons/optimism-icon";
+import PolygonIcon from "@workspace/ui/icons/polygon-icon";
 
 type ChainIconProps = {
   className?: string;
@@ -20,8 +24,38 @@ type ChainIconProps = {
 };
 
 export default function ChainIcon({ className, chainId }: ChainIconProps) {
-  const Icon = CHAIN_ICONS[chainId];
+  const Icon = getChainIcon(chainId);
 
-  // TODO: render fallback
-  return Icon ? <Icon className={cn("size-5", className)} /> : null;
+  return (
+    <div className={cn("size-5", className)}>
+      {Icon ? (
+        <Icon className="size-full" />
+      ) : (
+        <Circle className="text-muted-foreground size-full" />
+      )}
+    </div>
+  );
+
+  function getChainIcon(chainId: number) {
+    switch (chainId) {
+      case mainnet.id:
+      case sepolia.id:
+        return EthereumIcon;
+      case arbitrum.id:
+      case arbitrumSepolia.id:
+        return ArbitrumIcon;
+      case base.id:
+      case baseSepolia.id:
+        return BaseIcon;
+      case optimism.id:
+      case optimismSepolia.id:
+        return OptimismIcon;
+      case polygon.id:
+      case polygonAmoy.id:
+        return PolygonIcon;
+      default:
+        console.error(`Unknown chain id: ${chainId}`);
+        return Circle;
+    }
+  }
 }
